1c5740e964dd9eb70ab11d098de1da04abb8ff10
caua <cauacouto022@gmail.com>
2026-03-27 23:37:05 -0300

    "feat": adiciona classe principal da aplicaçao

---
+++ src/main/java/br/com/couto/paking_api/PakingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.couto.paking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PakingApiApplication.class, args);
+	}
+
+}
