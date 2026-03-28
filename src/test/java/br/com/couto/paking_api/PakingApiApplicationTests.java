7c5f7c32dabdf1ea9dbcd92d319e722253bbb67c
caua <cauacouto022@gmail.com>
2026-03-27 23:37:38 -0300

    "test": adiciona testes da aplicaçao

---
+++ src/test/java/br/com/couto/paking_api/PakingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.couto.paking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
