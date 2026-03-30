2bff74e249c672ef7a09f97cfa9a0195300013d3
caua <cauacouto022@gmail.com>
2026-03-29 23:14:04 -0300

    "feat" adiciona fidByuserId

--- src/main/java/br/com/couto/paking_api/repository/ParkingRepository.java
+++ src/main/java/br/com/couto/paking_api/repository/ParkingRepository.java
@@ -1,7 +1,14 @@
 package br.com.couto.paking_api.repository;
 
 import br.com.couto.paking_api.domin.ParkingModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface ParkingRepository extends JpaRepository<ParkingModel,Long> {
+
+
+    Page<ParkingModel> findByUserId(UUID userId,Pageable pageable);
 }
