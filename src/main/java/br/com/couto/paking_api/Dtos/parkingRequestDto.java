f240648c49246a6dadc3accaaec093fe750daca6
caua <cauacouto022@gmail.com>
2026-03-29 23:14:58 -0300

    "feat" adiciona  userId

--- src/main/java/br/com/couto/paking_api/Dtos/parkingRequestDto.java
+++ src/main/java/br/com/couto/paking_api/Dtos/parkingRequestDto.java
@@ -1,7 +1,11 @@
 package br.com.couto.paking_api.Dtos;
 
+import java.util.UUID;
+
 public record parkingRequestDto(String placa,
                                 String modelo,
                                 String marca,
-                                String cor) {
+                                String cor,
+                                UUID userId
+                                ) {
 }
--- src/main/java/br/com/couto/paking_api/Dtos/parkingResponseDto.java
+++ src/main/java/br/com/couto/paking_api/Dtos/parkingResponseDto.java
@@ -1,8 +1,28 @@
 package br.com.couto.paking_api.Dtos;
 
+import br.com.couto.paking_api.domin.ParkingModel;
+
+import java.util.UUID;
+
 public record parkingResponseDto(Long id,
-        String placa,
-         String modelo,
-         String marca,
-         String cor) {
+                                 String placa,
+                                 String modelo,
+                                 String marca,
+                                 String cor,
+                                 UUID userId
+
+                                 ) {
+
+
+public parkingResponseDto(ParkingModel parkingModel) {
+
+    this(parkingModel.getId(),
+            parkingModel.getPlaca(),
+            parkingModel.getModelo(),
+            parkingModel.getMarca(),
+            parkingModel.getCor(),
+            parkingModel.getUser().getId()
+
+    );
+}
 }
